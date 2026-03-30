1f59a601d87aaf5dce677fdab02c33ebe0ccb812
Robson Cesar <rubinhodiz@gmail.com>
2026-03-30 19:11:38 -0300

    criando funcionalidade no JS para gerar o QRCODE

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
-console.log("ola mundo");
+//pegando os elementos
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = document.querySelector("#qr-form input");
+
+const qrCodeImg = document.querySelector("#qr-code img");
+
+
+//fuções
+function generateQrCode(){
+    const qrCodeInputValue = qrCodeInput.value; //pegando o valor do campo.
+
+    if(!qrCodeInputValue) return;
+
+    qrCodeBtn.innerText = "Gerando código....";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+    qrCodeImg.addEventListener("load", ()=>{
+        container.classList.add("active");
+        qrCodeBtn.innerText = "Código criado....";
+    });
+}
+
+//Eventos
+
+qrCodeBtn.addEventListener("click", () => {
+    generateQrCode();
+});
+
+qrCodeInput.addEventListener("keydown", (e) => {
+    if(e.code === "Enter"){
+        generateQrCode();
+    }
+});
+
+//Limpar área do QR CODE
+qrCodeInput.addEventListener("keyup", () => {
+    if(!qrCodeInput.value){
+        container.classList.remove("active");
+        qrCodeBtn.innerText = "Gerar QR CODE";
+    }
+})
